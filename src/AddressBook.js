90c0945da9552896b177549a546bc82ae18cf80e
kneepdee <gabriel.kuslap@gmail.com>
2019-03-13 12:36:30 +0200

    Add getInitialContacts method

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -1,5 +1,15 @@
 function AddressBook() {
   this.contacts = []
+  this.initialComplete = false
+  this.getInitialContacts = function(cb) {
+    var self = this
+    selfTimeout(function() {
+      self.initialComplete = true
+      if (cb) {
+        return cb()
+      }
+    }, 3)
+  }
   this.addContact = function(contact) {
     this.contacts.push(contact)
   }
